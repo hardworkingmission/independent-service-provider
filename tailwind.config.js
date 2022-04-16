f16a9babe842a7833cec34750bf3c1bf272c8320
hardworkingmission <shakil20131101041@gmail.com>
2022-04-16 21:06:50 +0600

    firebase-and-necessary-packages-i-done

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}",'./node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+}
