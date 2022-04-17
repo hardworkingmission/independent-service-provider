3fc3bbb8e26d92ba44b52ad956e370217d648941
hardworkingmission <shakil20131101041@gmail.com>
2022-04-18 01:52:38 +0600

    mouse-effect-added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 
 const Service = ({service}) => {
     const{id,name,price,description,img}=service
+
+    //mouse enter out effect added
+    const [over,setOver]=useState('1')
+    const appStyle={
+        transform:`scale(${over})`,
+        transition: `transform .5s ease` 
+    }
     const navigate=useNavigate()
     return (
         <div className='col border border-2 rounded-lg relative'>
-            <div className="service-image">
-                <img src={process.env.PUBLIC_URL+`/service-images/${img}`} alt="" className='rounded-t-lg'/>
+            <div className="service-image"  onMouseEnter={()=>setOver('1.03')} onMouseLeave={()=>setOver('1')}>
+                <img src={process.env.PUBLIC_URL+`/service-images/${img}`} alt="" style={appStyle} className='rounded-t-lg'/>
             </div>
             <div className="service-info p-2 text-center">
                 <h1 className='text-xl font-bold'>{name}</h1>
